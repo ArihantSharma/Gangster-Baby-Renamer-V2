f4241ee8bd6fcf7a249becb11eb499e94fff9b8c
Arihant Sharma <71224541+ArihantSharma@users.noreply.github.com>
2023-12-26 19:58:47 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -4,7 +4,9 @@ import os
 from pyrogram import Client, filters
 from helper.date import add_date
 from helper.database import uploadlimit, usertype, addpre
-ADMIN = int(os.environ.get("ADMIN", 1484670284))
+
+ADMIN = [int(admin_id) for admin_id in os.environ.get("ADMIN", "1484670284").split()]
+
 log_channel = int(os.environ.get("LOG_CHANNEL", ""))
 
 
