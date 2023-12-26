3ab05e4f10bdee14b88bedb6f3537840653d7aa2
Arihant Sharma <71224541+ArihantSharma@users.noreply.github.com>
2023-12-26 15:41:23 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -5,59 +5,59 @@ from pyrogram import Client , filters
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
 	text = """**Free Plan User**
-	Daily  Upload limit 1.2GB
-	Price Rs 0
+	__Daily  Upload limit 1.2GB
+	Price Rs 0__
 	
 	**🪙 Silver Tier 🪙** 
-	Daily  Upload  limit 10GB
-	Price Rs 20 per Month
+	__Daily  Upload  limit 10GB
+	Price Rs 20 per Month__
 	
 	**💫 Gold Tier 💫**
-	Daily Upload limit 50GB
-	Price Rs 45 per Month
+	__Daily Upload limit 50GB
+	Price Rs 45 per Month__
 	
 	**💎 Diamond 💎**
-	Daily Upload limit 100GB
-	Price Rs 80 per Month
+	__Daily Upload limit 100GB
+	Price Rs 80 per Month__
 	
 	
 	Pay Using Upi I'd `arihantsharmaofficial@oksbi`
 	
-	After Payment Send Screenshots Of 
-        Payment To Admins"""
+	```After Payment Send Screenshots Of 
+        Payment To Admins```"""
 	keybord = InlineKeyboardMarkup([[ 
         			InlineKeyboardButton("ᴀᴅᴍɪɴ 1",url = "https://t.me/iwilltouchyoulilnigga"),InlineKeyboardButton("ᴀᴅᴍɪɴ 2",url = "https://t.me/Sixteen_Years_Older")], 
-        			[InlineKeyboardButton("20₹",url = "upi://pay?pa=arihantsharmaofficial@oksbi&pn=Arihant%20Sharma&am=20.00&cu=INR&aid=uGICAgMDAwIqrXQ"),
-        			InlineKeyboardButton("45₹",url = "upi://pay?pa=arihantsharmaofficial@oksbi&pn=Arihant%20Sharma&am=45.00&cu=INR&aid=uGICAgMDAwIqrXQ"),
-        			InlineKeyboardButton("80₹",url = "upi://pay?pa=arihantsharmaofficial@oksbi&pn=Arihant%20Sharma&am=80.00&cu=INR&aid=uGICAgMDAwIqrXQ")],[InlineKeyboardButton("ᴄᴀɴᴄᴇʟ",callback_data = "cancel")  ]])
+        			[InlineKeyboardButton("20₹",url = "https://pay.upilink.in/pay/arihantsharmaofficial@oksbi?am=20"),
+        			InlineKeyboardButton("45₹",url = "https://pay.upilink.in/pay/arihantsharmaofficial@oksbi?am=45"),
+        			InlineKeyboardButton("80₹",url = "https://pay.upilink.in/pay/arihantsharmaofficial@oksbi?am=80")],[InlineKeyboardButton("ᴄᴀɴᴄᴇʟ",callback_data = "cancel")  ]])
 	await update.message.edit(text = text,reply_markup = keybord)
 	
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
 	text = """**Free Plan User**
-	Daily  Upload limit 1.2GB
-	Price Rs 0
+	__Daily  Upload limit 1.2GB
+	Price Rs 0__
 	
 	**🪙 Silver Tier 🪙** 
-	Daily  Upload  limit 10GB
-	Price Rs 20 per Month
+	__Daily  Upload  limit 10GB
+	Price Rs 20 per Month__
 	
 	**💫 Gold Tier 💫**
-	Daily Upload limit 50GB
-	Price Rs 45 per Month
+	__Daily Upload limit 50GB
+	Price Rs 45 per Month__
 	
 	**💎 Diamond 💎**
-	Daily Upload limit 100GB
-	Price Rs 80 per Month
+	__Daily Upload limit 100GB
+	Price Rs 80 per Month__
 	
 	
 	Pay Using Upi I'd `arihantsharmaofficial@oksbi`
 	
-	After Payment Send Screenshots Of 
-        Payment To Admins"""
+	```After Payment Send Screenshots Of 
+        Payment To Admins```"""
 	keybord = InlineKeyboardMarkup([[ 
         			InlineKeyboardButton("ᴀᴅᴍɪɴ 1",url = "https://t.me/iwilltouchyoulilnigga"),InlineKeyboardButton("ᴀᴅᴍɪɴ 2",url = "https://t.me/Sixteen_Years_Older")], 
-        			[InlineKeyboardButton("20₹",url = "upi://pay?pa=arihantsharmaofficial@oksbi&pn=Arihant%20Sharma&am=20.00&cu=INR&aid=uGICAgMDAwIqrXQ"),
-        			InlineKeyboardButton("45₹",url = "upi://pay?pa=arihantsharmaofficial@oksbi&pn=Arihant%20Sharma&am=45.00&cu=INR&aid=uGICAgMDAwIqrXQ"),
-        			InlineKeyboardButton("80₹",url = "upi://pay?pa=arihantsharmaofficial@oksbi&pn=Arihant%20Sharma&am=80.00&cu=INR&aid=uGICAgMDAwIqrXQ")],[InlineKeyboardButton("ᴄᴀɴᴄᴇʟ",callback_data = "cancel")  ]])
+        			[InlineKeyboardButton("20₹",url = "https://pay.upilink.in/pay/arihantsharmaofficial@oksbi?am=20"),
+        			InlineKeyboardButton("45₹",url = "https://pay.upilink.in/pay/arihantsharmaofficial@oksbi?am=45"),
+        			InlineKeyboardButton("80₹",url = "https://pay.upilink.in/pay/arihantsharmaofficial@oksbi?am=80")],[InlineKeyboardButton("ᴄᴀɴᴄᴇʟ",callback_data = "cancel")  ]])
