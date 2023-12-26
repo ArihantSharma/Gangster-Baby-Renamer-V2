b3097da897eca7b8882f9b20a1618cd345347d17
Arihant Sharma <71224541+ArihantSharma@users.noreply.github.com>
2023-12-26 17:48:00 +0530

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -8,22 +8,22 @@ async def add_caption(client, message):
        return await message.reply_text("**Give me a caption to set.\n\nExample:- `/set_caption File Name`**")
     caption = message.text.split(" ", 1)[1]
     addcaption(int(message.chat.id), caption)
-    await message.reply_text("**Your Caption successfully added ✅**")
+    await message.reply_text("**ʏᴏᴜʀ ᴄᴀᴘᴛɪᴏɴ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ᴀᴅᴅᴇᴅ ✅**")
 
 @Client.on_message(filters.private & filters.command('del_caption'))
 async def delete_caption(client, message): 
     caption = find(int(message.chat.id))[1]
     if not caption:
-        await message.reply_text("**You dont have any custom caption**")
+        await message.reply_text("**ʏᴏᴜ ᴅᴏɴᴛ ʜᴀᴠᴇ ᴀɴʏ ᴄᴜꜱᴛᴏᴍ ᴄᴀᴘᴛɪᴏɴ**")
         return
     delcaption(int(message.chat.id))
-    await message.reply_text("**Your caption successfully deleted ✅**")
+    await message.reply_text("**ʏᴏᴜʀ ᴄᴀᴘᴛɪᴏɴ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟʏ ᴅᴇʟᴇᴛᴇᴅ ✅**")
                                        
 @Client.on_message(filters.private & filters.command('see_caption'))
 async def see_caption(client, message): 
     caption = find(int(message.chat.id))[1]
     if caption:
-       await message.reply_text(f"<b><u>Your Caption:</b></u>\n\n`{caption}`")
+       await message.reply_text(f"<b><u>ʏᴏᴜʀ ᴄᴀᴘᴛɪᴏɴ :</b></u>\n\n`{caption}`")
     else:
-       await message.reply_text("**You dont have any custom caption**")
+       await message.reply_text("**ʏᴏᴜ ᴅᴏɴᴏᴛ ʜᴀᴠᴇ ᴀɴʏ ᴄᴜꜱᴛᴏᴍ ᴄᴀᴘᴛɪᴏɴ**")
           
